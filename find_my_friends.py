f7c989b570f37078e94c4cb16f6f56c3ec6d98cc
Muhammad Qasim Omer <mqasimomer@gmail.com>
2024-07-01 19:00:43 +0500

    Create find_my_friends.py

---
+++ find_my_friends.py
@@ -0,0 +1,15 @@
+#code
+
+# Write code below 💖
+mylocation = (31.5204, 74.3587)
+friend1_location = (30.8138, 73.4534)
+friend2_location = (30.6682, 73.1114)
+friend3_location = (32.5731, 74.1005)
+
+location_tuple = (
+  (31.5204, 74.3587),
+  (30.8138, 73.4534),
+  (30.6682, 73.1114),
+  (32.5731, 74.1005)
+)
+print(location_tuple)
