3ce07f0a1132f18373e550df270963682cc3868a
Muhammad Qasim Omer <mqasimomer@gmail.com>
2024-07-01 19:14:18 +0500

    Create fruit_cart.py

---
+++ fruit_cart.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+my_fav_fruits = {'apple', 'mango', 'banana'}
+my_bestie_fav_fruits = {'orange', 'watermelon', 'cherry'}
+
+#union result
+union_results = my_fav_fruits.union(my_bestie_fav_fruits)
+
+#interection results
+intersection_results = my_fav_fruits.intersection(my_bestie_fav_fruits)
+
+#print results
+print(union_results)
+print(intersection_results)
